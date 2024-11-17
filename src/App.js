3c805fd38f72f5dc7cfe4871b8ca3534c08f75bc
ohad1881 <tbhani123@gmail.com>
2024-11-17 16:39:04 +0300

    inital demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
+
 export default function App() {
+  const [numbersArr, setNumbersArr] = useState([]);
+
+  function handleRoll() {
+    const tempArr = [0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i <= 5; i++) {
+      tempArr[i] = Math.floor(Math.random() * 37) + 1;
+    }
+    tempArr[6] = Math.floor(Math.random() * 7) + 1;
+    setNumbersArr(tempArr);
+  }
   return (
     <div className="app">
-      <div>Hello nigga</div>
-      <div>Hello nigga</div>
-      <div>Hello nigga</div>
-      <div>Hello nigga</div>
+      <Title />
+      <Randomize Numbers={numbersArr} HandleRoll={handleRoll} />
+    </div>
+  );
+}
+
+function Title() {
+  return <div className="title">Lotomat</div>;
+}
+
+function Randomize({ Numbers, HandleRoll }) {
+  return (
+    <div className="main">
+      <span className="numbers">
+        {" "}
+        {Numbers.length > 0
+          ? Numbers.join(" | ")
+          : "Click ROLL to generate numbers!"}
+      </span>
+      <button className="button" onClick={HandleRoll}>
+        ROLL!{" "}
+      </button>
     </div>
   );
 }
